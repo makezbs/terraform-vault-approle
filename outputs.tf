6f2f0aa3ce1b7103a1ab6666d6492088342391f4
Igor Churmeev <ingvarch@gmail.com>
2020-03-09 17:28:58 +0200

    Fix output path to value
    Signed-off-by: Igor Churmeev <ingvarch@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "role_id" {
-  value = vault_approle_auth_backend_role.role_id
+  value = vault_approle_auth_backend_role.this.role_id
 }
 
 output "secret_id" {
-  value = vault_approle_auth_backend_role_secret_id.secret_id
+  value = vault_approle_auth_backend_role_secret_id.this.secret_id
 }
